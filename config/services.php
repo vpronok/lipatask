e9253c4ac0ea57cbeb22d2ed2af3d3acccf849ae
VK <v.pronok@gmail.com>
2026-05-02 16:05:59 -0400

    changes to services.php

--- config/services.php
+++ config/services.php
@@ -29,10 +29,10 @@ return [
     ],
 
     'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
+       'notifications' => [
+           'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
+           'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+       ],
     ],
     // --- PAYHERO REGISTRATION GLOBALLY ---
     'payhero' =>[
