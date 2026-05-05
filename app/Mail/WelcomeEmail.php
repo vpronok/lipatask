f3400dd83a607b3f6ea0534fcfd16471d38f251d
VK <v.pronok@gmail.com>
2026-05-05 07:39:16 -0400

    email fix

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -1,79 +1,40 @@
 <?php
+
 namespace App\Mail;
+
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Address;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
+
     public $user;
 
-    public function __construct($user) {
+    public function __construct($user)
+    {
         $this->user = $user;
     }
 
-    public function build() {
-        return $this->subject('Welcome to Chatwazungu! 🚀')
-                    ->view('emails.welcome');
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            from: new Address(env('MAIL_FROM_ADDRESS', 'notifications@chatwazungu.com'), env('MAIL_FROM_NAME', 'Chatwazungu')),
+            replyTo:[
+                new Address(env('MAIL_FROM_ADDRESS', 'notifications@chatwazungu.com'), env('MAIL_FROM_NAME', 'Chatwazungu')),
+            ],
+            subject: 'Welcome to Chatwazungu! 🚀',
+        );
+    }
+
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.welcome',
+        );
     }
 }
-
-
-
-// <?php
-
-// namespace App\Mail;
-
-// use Illuminate\Bus\Queueable;
-// use Illuminate\Contracts\Queue\ShouldQueue;
-// use Illuminate\Mail\Mailable;
-// use Illuminate\Mail\Mailables\Attachment;
-// use Illuminate\Mail\Mailables\Content;
-// use Illuminate\Mail\Mailables\Envelope;
-// use Illuminate\Queue\SerializesModels;
-
-// class WelcomeEmail extends Mailable
-// {
-//     use Queueable, SerializesModels;
-
-//     /**
-//      * Create a new message instance.
-//      */
-//     public function __construct()
-//     {
-//         //
-//     }
-
-//     /**
-//      * Get the message envelope.
-//      */
-//     public function envelope(): Envelope
-//     {
-//         return new Envelope(
-//             subject: 'Welcome Email',
-//         );
-//     }
-
-//     /**
-//      * Get the message content definition.
-//      */
-//     public function content(): Content
-//     {
-//         return new Content(
-//             view: 'view.name',
-//         );
-//     }
-
-//     /**
-//      * Get the attachments for the message.
-//      *
-//      * @return array<int, Attachment>
-//      */
-//     public function attachments(): array
-//     {
-//         return [];
-//     }
-// }
-
-
